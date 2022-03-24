31421b2492aaa02585d8a88de54a736a71f25483
Neil Weidinger <neilweidingern@gmail.com>
2022-03-24 15:21:20 +0000

    Update quicksort benchmark

--- benches/quicksort.rs
+++ benches/quicksort.rs
@@ -3,38 +3,57 @@ use benchmarks::{Parallel, Serial};
 use criterion::BatchSize::SmallInput;
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
+fn inputs() -> Vec<Vec<i32>> {
+    [1_000, 1_000_000, 10_000_000]
+        .iter()
+        .map(|&len| generate_random_sequence(len))
+        .collect()
+}
+
 fn quicksort_bench(c: &mut Criterion) {
+    fn param_string(cores: usize, len: usize) -> String {
+        format!("Cores - {} Len - {}", cores, len)
+    }
+
     let mut quicksort_group = c.benchmark_group("Quicksort");
     let step = if num_cpus::get() <= 10 { 2 } else { 5 };
     let num_cores = [1]
         .into_iter()
         .chain((step..=num_cpus::get()).step_by(step));
+    let inputs = inputs();
 
-    for len in [1_000, 1_000_000, 10_000_000] {
-        for cores in num_cores.clone() {
-            let pool = rayon::ThreadPoolBuilder::new()
-                .num_threads(cores)
-                .build()
-                .unwrap();
-
-            let param_string = format!("Cores - {} Len - {}", cores, len);
-            let input = generate_random_sequence(len);
-
-            quicksort_group.bench_function(BenchmarkId::new("Serial", param_string.clone()), |b| {
+    for input in inputs.iter() {
+        // Bench serial version once for each len, no need to loop through cores since serial
+        // version only runs on one core (doesn't hook into Rayon)
+        quicksort_group.bench_function(
+            BenchmarkId::new("Serial", param_string(1, input.len())),
+            |b| {
                 b.iter_batched_ref(
                     || input.clone(),
-                    |i| quicksort::<_, Serial>(black_box(i), 0, 0.0), // no need for pool since serial version shouldn't hook into rayon
+                    |i| quicksort::<_, Serial>(black_box(i), 0, 0.0), // no need for pool since serial version doesn't hook into Rayon
                     SmallInput,
                 );
-            });
+            },
+        );
+    }
 
-            quicksort_group.bench_function(BenchmarkId::new("Parallel", param_string), |b| {
-                b.iter_batched_ref(
-                    || input.clone(),
-                    |i| pool.install(|| quicksort::<_, Parallel>(black_box(i), 0, 0.0)),
-                    SmallInput,
-                );
-            });
+    for cores in num_cores.clone() {
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(cores)
+            .build()
+            .unwrap();
+
+        for input in inputs.iter() {
+            quicksort_group.bench_function(
+                BenchmarkId::new("Parallel", param_string(cores, input.len())),
+                |b| {
+                    b.iter_batched_ref(
+                        || input.clone(),
+                        |i| pool.install(|| quicksort::<_, Parallel>(black_box(i), 0, 0.0)),
+                        SmallInput,
+                    );
+                },
+            );
         }
     }
 }
